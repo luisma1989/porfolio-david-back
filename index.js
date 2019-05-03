196937975cf7793f2532d0fcfb084bfd75d3f82f
Luisma <luismanuel.fernandez.camacho@gmail.com>
2019-05-03 08:22:38 +0200

    feat: add cors

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const app = express()
 const api = require('./routes')
 const config = require('./config')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.get('/', (req, res) => {
-  res.status(200).send('Hola')
-})
+app.use(cors({
+  credentials: true,
+}))
 
 app.use('images', express.static('images'))
 app.use('/api', api)
 
-mongoose.connect(config.db, (err, res) => {
-  if (err) {
-    return console.log(`Error al conectar a la base de datos: ${err}`)
-  }
-  console.log('Conexión a la base de datos establecida...')
+mongoose.connect(
+  config.db,
+  { useNewUrlParser: true },
+  (err) => {
+    if (err) {
+      return console.log(`Error al conectar a la base de datos: ${err}`)
+    }
+    console.log('Conexión a la base de datos establecida...')
 
-  app.listen(config.port, () => {
-    console.log(`API REST corriendo en http://localhost:${config.port}`)
-  })
-})
+    app.listen(config.port, () => {
+      console.log(`API REST corriendo en http://localhost:${config.port}`)
+    })
+  }
+)
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,10 @@ const api = express.Router()
 api.get('/header', homeController.getHeader)
 api.post('/header', homeController.setHeader)
 api.get('/subheader', homeController.getSubheader)
+api.post('/subheader', homeController.setSubheader)
 api.get('/countries', homeController.getCountries)
+api.post('/countries', homeController.setCountries)
 api.get('/experience', homeController.getExperience)
+api.post('/experience', homeController.setExperience)
 
 module.exports = api
